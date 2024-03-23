729038b1de3a7b82e2dcfacaf13ac70216be64a0
mathewm24 <1mathewnoel@gmail.com>
2024-03-23 14:58:28 +0800

    updates

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
 
+class Node {
+    public:
+    int Value;
+    Node* Next;
+};
+
+void printList(Node* n){
+    while(n!= nullptr){
+        std::cout << n->Value << std::endl;
+        n = n->Next;
+    }
+}
+
+int main(){
+    
+    return 0;
+}
