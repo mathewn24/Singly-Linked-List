57a52d6f6dcd82540b784c078432a553c9f43b09
mathewm24 <1mathewnoel@gmail.com>
2024-03-25 01:02:17 +0800

    completed linked list.

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -13,7 +13,66 @@ void printList(Node* n){
     }
 }
 
-int main(){
+void insertAtHead(Node** head, int newValue) {
+    // Prepare new node.
+    Node* newHead = new Node();
+    newHead -> Value = newValue;
+
+    // Place node in front of existing head.
+    newHead->Next = *head;
+
+    // Move head to the new node.
+    *head = newHead;
+}
+
+void insertAtTail(Node** head, int newVal) {
+    // Prepare the node.
+    Node* newTail = new Node();
+    newTail->Value = newVal;
+    newTail->Next = nullptr;
     
+    // If linked list is empty, new node will become the head.
+    if (*head == nullptr) {
+        *head = newTail;
+    }
+    // Find the last node.
+    Node* end = *head;
+    while (end->Next) {
+        end = end->Next;
+    }
+    // Place node after the end node.
+    end->Next = newTail;
+}
+
+void insertAtNode(Node* givenNode, int newVal) {
+    // Check if node exists.
+    if (givenNode == nullptr) {
+        std::cout << "The node cannot be null.";
+    }
+
+    // Prepare node to be inserted.
+    Node* newNode = new Node();
+    newNode->Value = newVal;
+
+    // Insert new node after given.
+    newNode->Next = givenNode->Next;
+    givenNode->Next = newNode;
+
+}
+
+int main(){
+
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->Value = 1;
+    head->Next = second;
+    second->Value = 2;
+    second->Next = third;
+    third->Value = 3;
+    third->Next = nullptr;
+    printList(head);
+
     return 0;
 }
